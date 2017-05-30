27799cfb16793e39e3611e7849271573b54758ab
Dmitry Malinovsky <damalinov@gmail.com>
2017-05-30 07:52:54 +0600

    Pass all open_connection arguments

--- amqproto/io/asyncio.py
+++ amqproto/io/asyncio.py
@@ -45,6 +45,8 @@ class Channel(SansioChannel):
 class Connection(SansioConnection):
 
     def __init__(self, host='localhost', port=5672, *,
+                 limit=None, ssl=None, family=0, proto=0,
+                 flags=0, sock=None, local_addr=None, server_hostname=None,
                  loop: asyncio.BaseEventLoop, **kwargs):
         self.loop = loop
         # Late call because we need self.Future available.
@@ -52,9 +54,12 @@ class Connection(SansioConnection):
 
         async def open_connection():
             self.reader, self.writer = await asyncio.open_connection(
-                host=host, port=port, loop=loop
+                host=host, port=port, loop=loop, limit=limit,
+                ssh=ssl, family=family, proto=proto, flags=flags,
+                sock=sock, local_addr=local_addr,
+                server_hostname=server_hostname,
             )
-            loop.create_task(self.communicate())
+            loop.create_task(self._communicate())
         self._open_connection = open_connection
 
     def Future(self):
@@ -63,7 +68,7 @@ class Connection(SansioConnection):
     def Channel(self, *args, **kwargs):
         return Channel(*args, loop=self.loop, writer=self.writer, **kwargs)
 
-    async def communicate(self):
+    async def _communicate(self):
         data = b''
         reader, writer = self.reader, self.writer
         while self.alive:
