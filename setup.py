703e29acdd43b34b73fabbd2b0375a453df4ed28
Dmitry Malinovsky <damalinov@gmail.com>
2017-07-31 23:28:48 +0600

    Switch to pbr

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import setuptools
 
 
 setuptools.setup(
-    name='amqproto',
-    version='0.1',
-    license='BSD 3-clause',
-    packages=setuptools.find_packages(exclude='tests'),
+    setup_requires=['pbr'],
+    pbr=True,
 )
